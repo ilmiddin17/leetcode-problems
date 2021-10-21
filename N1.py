20eb6765ca01e10ed4391d9d696dc8bcbe8e1a3a
Ilmiddin Tuychiev <79243649+ilmiddin17@users.noreply.github.com>
2021-10-21 09:25:50 +0500

    updated leetcode #1

--- N1.py
+++ N1.py
@@ -1,3 +1,14 @@
+#Fist solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i, num in enumerate(nums):
+            m = target - num
+            if m in dict:
+                return [dict[m], i]
+            else:
+                dict[num] = i
+#Second solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         i=0
